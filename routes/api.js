96890b694fed5f9f3475776140c4170e49b0f1e2
Franco Di Masi <Franco@Francos-MacBook-Pro.local>
2016-01-09 17:38:40 -0300

    add routes

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.send('Twitter App for Santi');
+// });
+
+// GET Twits
+router.get('/twits', function(req, res, next) {
+  res.send('Test Api');
+});
+
+module.exports = router;
