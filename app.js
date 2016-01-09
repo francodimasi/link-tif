c3463f8376e72ca90384a700ff27255afb5a3eda
Franco Di Masi <Franco@Francos-MacBook-Pro.local>
2016-01-09 17:35:00 -0300

    add node

---
+++ app.js
@@ -0,0 +1,44 @@
+// Dependency Modules
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cfenv = require('cfenv');
+var moment = require('moment');
+var debug = require('debug');
+
+var routes = require('./routes/api');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Mount Middleware
+app.use('/', routes);
+
+// Set Port.
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Create HTTP server.
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Normalize a port into a number, string, or false.
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+}
